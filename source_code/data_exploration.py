20ded5fb82ec4d30cb7e2e095a6b9294397d6bf8
jgartman <joshgartman@gmail.com>
2017-04-27 21:32:10 -0400

    refinements to paper

--- source_code/data_exploration.py
+++ source_code/data_exploration.py
@@ -5,83 +5,157 @@ Created on Sat Mar 18 21:55:50 2017
 @author: Josh
 """
 import pandas
+import pickle
 import matplotlib.pyplot as plt
 
-data = pandas.read_csv("../higgs_data/atlas-higgs-challenge-2014-v2.csv")
+#data = pandas.read_csv("../higgs_data/atlas-higgs-challenge-2014-v2.csv")
+#
+#s = data[data["Label"] == "s"]
+#b = data[data["Label"] == "b"]
+#
+#high_level_features = [
+# 'DER_mass_MMC',
+# 'DER_mass_transverse_met_lep',
+# 'DER_mass_vis',
+# 'DER_pt_h',
+# 'DER_deltar_tau_lep',
+# 'DER_pt_tot',
+# 'DER_sum_pt',
+# 'DER_pt_ratio_lep_tau',
+# 'DER_met_phi_centrality',
+# 'DER_lep_eta_centrality']
+#
+#low_level_features = [
+# 'PRI_tau_pt',
+# 'PRI_tau_eta',
+# 'PRI_tau_phi',
+# 'PRI_lep_pt',
+# 'PRI_lep_eta',
+# 'PRI_lep_phi',
+# 'PRI_met',
+# 'PRI_met_phi',
+# 'PRI_met_sumet',
+# 'PRI_jet_num',
+# 'PRI_jet_leading_pt',
+# 'PRI_jet_all_pt']
+#
+#plt.hist(s[s["DER_mass_MMC"] != -999]["DER_mass_MMC"],bins=50,histtype="step",range=(0,500),color="blue",normed=True)
+#plt.hist(b[b["DER_mass_MMC"] != -999]["DER_mass_MMC"],bins=50,histtype="step",range=(0,500),color="green",normed=True)
+#
+#plt.title('Estimated mass of Higgs boson candidate')
+#plt.xlabel('GeV')
+#plt.savefig('../plots/DER_mass_MMC.png')
+#plt.show()
+#
+#plt.hist(s["DER_mass_transverse_met_lep"], bins=50,range=(0,300),histtype="step",color="blue",normed=True)
+#plt.hist(b["DER_mass_transverse_met_lep"], bins=50,range=(0,300),histtype="step",color="green",normed=True)
+#
+#plt.title('Transverse mass')
+#plt.xlabel('GeV')
+#plt.savefig('../plots/DER_mass_transverse_met_lep.png')
+#plt.show()
+#
+#plt.hist(s["DER_sum_pt"], bins=50,range=(0,500),histtype="step",color="blue",normed=True)
+#plt.hist(b["DER_sum_pt"], bins=50,range=(0,500),histtype="step",color="green",normed=True)
+#
+#plt.title('Sum of modulii of transverse momenta')
+#plt.xlabel('GeV')
+#plt.savefig('../plots/DER_sum_pt.png')
+#plt.show()
+#
+#plt.hist(s["PRI_tau_pt"], bins=50,range=(0,500),histtype="step",color="blue",normed=True)
+#plt.hist(b["PRI_tau_pt"], bins=50,range=(0,500),histtype="step",color="green",normed=True)
+#
+#plt.title('Transverse momenta of the tau')
+#plt.xlabel('GeV')
+#plt.savefig('../plots/PRI_tau_pt.png')
+#plt.show()
+#
+#plt.hist(s["PRI_lep_pt"], bins=50,range=(0,500),histtype="step",color="blue",normed=True)
+#plt.hist(b["PRI_lep_pt"], bins=50,range=(0,500),histtype="step",color="green",normed=True)
+#
+#plt.title('Transverse momenta of the lepton (electron or muon)')
+#plt.xlabel('GeV')
+#plt.savefig('../plots/PRI_lep_pt.png')
+#plt.show()
+#
+#plt.hist(s["PRI_met"], bins=50,range=(0,200),histtype="step",color="blue",normed=True)
+#plt.hist(b["PRI_met"], bins=50,range=(0,200),histtype="step",color="green",normed=True)
+#
+#plt.title('Missing transverse energy')
+#plt.xlabel('GeV')
+#plt.savefig('../plots/PRI_met.png')
+#plt.show()
 
-s = data[data["Label"] == "s"]
-b = data[data["Label"] == "b"]
+#der_train_accuracy_f = open('./output/2017-04-23-13-57-35-390150/train_accuracy.p')
+#pri_train_accuracy_f = open('./output/2017-04-23-20-02-26-675737/train_accuracy.p')
+#all_train_accuracy_f = open('./output/2017-04-24-02-24-35-708578/train_accuracy.p')
+#
+#der_test_accuracy_f = open('./output/2017-04-23-13-57-35-390150/test_accuracy.p')
+#pri_test_accuracy_f = open('./output/2017-04-23-20-02-26-675737/test_accuracy.p')
+#all_test_accuracy_f = open('./output/2017-04-24-02-24-35-708578/test_accuracy.p')
+#
+#der_accuracy_train = pickle.load(der_train_accuracy_f)
+#pri_accuracy_train = pickle.load(pri_train_accuracy_f)
+#all_accuracy_train = pickle.load(all_train_accuracy_f)
+#
+#der_accuracy_test = pickle.load(der_test_accuracy_f)
+#pri_accuracy_test = pickle.load(pri_test_accuracy_f)
+#all_accuracy_test = pickle.load(all_test_accuracy_f)
+#
+#plt.plot(range(len(der_accuracy_test)), der_accuracy_test, color='red')
+#plt.plot(range(len(pri_accuracy_test)), pri_accuracy_test, color='blue')
+#plt.plot(range(len(all_accuracy_test)), all_accuracy_test, color='green')
+#plt.ylim([.70,.84])
+#plt.xlim([0,2000])
+#plt.xlabel('Training Epochs')
+#plt.ylabel('Accuracy')
+#plt.savefig('../plots/accuracy.png')
+#
+#
+#der_test_auc_f = open('./output/2017-04-23-13-57-35-390150/test_auc.p')
+#pri_test_auc_f = open('./output/2017-04-23-20-02-26-675737/test_auc.p')
+#all_test_auc_f = open('./output/2017-04-24-02-24-35-708578/test_auc.p')
+#
+#der_auc_test = pickle.load(der_test_auc_f)
+#pri_auc_test = pickle.load(pri_test_auc_f)
+#all_auc_test = pickle.load(all_test_auc_f)
 
-high_level_features = [
- 'DER_mass_MMC',
- 'DER_mass_transverse_met_lep',
- 'DER_mass_vis',
- 'DER_pt_h',
- 'DER_deltar_tau_lep',
- 'DER_pt_tot',
- 'DER_sum_pt',
- 'DER_pt_ratio_lep_tau',
- 'DER_met_phi_centrality',
- 'DER_lep_eta_centrality']
+#plt.plot(range(len(der_accuracy)), pri_accuracy, color='green')
+#plt.plot(range(len(der_accuracy)), all_accuracy, color='red')
+#plt.ylim([.70,.84])
 
-low_level_features = [
- 'PRI_tau_pt',
- 'PRI_tau_eta',
- 'PRI_tau_phi',
- 'PRI_lep_pt',
- 'PRI_lep_eta',
- 'PRI_lep_phi',
- 'PRI_met',
- 'PRI_met_phi',
- 'PRI_met_sumet',
- 'PRI_jet_num',
- 'PRI_jet_leading_pt',
- 'PRI_jet_all_pt']
+der_train_accuracy_f = open('./output/2017-04-26-01-58-34-114255/train_accuracy.p')
+pri_train_accuracy_f = open('./output/2017-04-26-10-16-41-095791/train_accuracy.p')
+all_train_accuracy_f = open('./output/2017-04-26-18-51-54-310030/train_accuracy.p')
 
-plt.hist(s[s["DER_mass_MMC"] != -999]["DER_mass_MMC"],bins=50,histtype="step",range=(0,500),color="blue",normed=True)
-plt.hist(b[b["DER_mass_MMC"] != -999]["DER_mass_MMC"],bins=50,histtype="step",range=(0,500),color="green",normed=True)
+der_test_accuracy_f = open('./output/2017-04-26-01-58-34-114255/test_accuracy.p')
+pri_test_accuracy_f = open('./output/2017-04-26-10-16-41-095791/test_accuracy.p')
+all_test_accuracy_f = open('./output/2017-04-26-18-51-54-310030/test_accuracy.p')
 
-plt.title('Estimated mass of Higgs boson candidate')
-plt.xlabel('GeV')
-plt.savefig('../plots/DER_mass_MMC.png')
-plt.show()
+der_accuracy_train = pickle.load(der_train_accuracy_f)
+pri_accuracy_train = pickle.load(pri_train_accuracy_f)
+all_accuracy_train = pickle.load(all_train_accuracy_f)
 
-plt.hist(s["DER_mass_transverse_met_lep"], bins=50,range=(0,300),histtype="step",color="blue",normed=True)
-plt.hist(b["DER_mass_transverse_met_lep"], bins=50,range=(0,300),histtype="step",color="green",normed=True)
+der_accuracy_test = pickle.load(der_test_accuracy_f)
+pri_accuracy_test = pickle.load(pri_test_accuracy_f)
+all_accuracy_test = pickle.load(all_test_accuracy_f)
 
-plt.title('Transverse mass')
-plt.xlabel('GeV')
-plt.savefig('../plots/DER_mass_transverse_met_lep.png')
-plt.show()
+plt.plot(range(len(der_accuracy_test)), der_accuracy_test, color='red')
+plt.plot(range(len(pri_accuracy_test)), pri_accuracy_test, color='blue')
+plt.plot(range(len(all_accuracy_test)), all_accuracy_test, color='green')
+plt.ylim([.70,.84])
+plt.xlim([0,2000])
+plt.xlabel('Training Epochs')
+plt.ylabel('Accuracy')
+plt.savefig('../plots/accuracy_nn.png')
 
-plt.hist(s["DER_sum_pt"], bins=50,range=(0,500),histtype="step",color="blue",normed=True)
-plt.hist(b["DER_sum_pt"], bins=50,range=(0,500),histtype="step",color="green",normed=True)
 
-plt.title('Sum of modulii of transverse momenta')
-plt.xlabel('GeV')
-plt.savefig('../plots/DER_sum_pt.png')
-plt.show()
+der_test_auc_f = open('./output/2017-04-26-01-58-34-114255/test_auc.p')
+pri_test_auc_f = open('./output/2017-04-26-10-16-41-095791/test_auc.p')
+all_test_auc_f = open('./output/2017-04-26-18-51-54-310030/test_auc.p')
 
-plt.hist(s["PRI_tau_pt"], bins=50,range=(0,500),histtype="step",color="blue",normed=True)
-plt.hist(b["PRI_tau_pt"], bins=50,range=(0,500),histtype="step",color="green",normed=True)
+der_auc_test = pickle.load(der_test_auc_f)
+pri_auc_test = pickle.load(pri_test_auc_f)
+all_auc_test = pickle.load(all_test_auc_f)
 
-plt.title('Transverse momenta of the tau')
-plt.xlabel('GeV')
-plt.savefig('../plots/PRI_tau_pt.png')
-plt.show()
-
-plt.hist(s["PRI_lep_pt"], bins=50,range=(0,500),histtype="step",color="blue",normed=True)
-plt.hist(b["PRI_lep_pt"], bins=50,range=(0,500),histtype="step",color="green",normed=True)
-
-plt.title('Transverse momenta of the lepton (electron or muon)')
-plt.xlabel('GeV')
-plt.savefig('../plots/PRI_lep_pt.png')
-plt.show()
-
-plt.hist(s["PRI_met"], bins=50,range=(0,200),histtype="step",color="blue",normed=True)
-plt.hist(b["PRI_met"], bins=50,range=(0,200),histtype="step",color="green",normed=True)
-
-plt.title('Missing transverse energy')
-plt.xlabel('GeV')
-plt.savefig('../plots/PRI_met.png')
-plt.show()
--- source_code/hyper_opt.py
+++ source_code/hyper_opt.py
@@ -61,46 +61,36 @@ def main(job_id,params):
 
     del data
 
-    features = ['DER_mass_MMC', 'DER_mass_transverse_met_lep',
-                'DER_mass_vis', 'DER_pt_h', 'DER_deltaeta_jet_jet', 'DER_mass_jet_jet',
-                'DER_prodeta_jet_jet', 'DER_deltar_tau_lep', 'DER_pt_tot', 'DER_sum_pt',
-                'DER_pt_ratio_lep_tau', 'DER_met_phi_centrality',
-                'DER_lep_eta_centrality', 'PRI_tau_pt', 'PRI_tau_eta', 'PRI_tau_phi',
-                'PRI_lep_pt', 'PRI_lep_eta', 'PRI_lep_phi', 'PRI_met', 'PRI_met_phi',
-                'PRI_met_sumet', 'PRI_jet_num', 'PRI_jet_leading_pt',
-                'PRI_jet_leading_eta', 'PRI_jet_leading_phi', 'PRI_jet_subleading_pt',
-                'PRI_jet_subleading_eta', 'PRI_jet_subleading_phi', 'PRI_jet_all_pt']
-
+    high_level_features = [
+     'DER_mass_MMC',
+     'DER_mass_transverse_met_lep',
+     'DER_mass_vis',
+     'DER_pt_h',
+     'DER_deltar_tau_lep',
+     'DER_pt_tot',
+     'DER_sum_pt',
+     'DER_pt_ratio_lep_tau',
+     'DER_met_phi_centrality',
+     'DER_lep_eta_centrality']
     
-    undefined_columns = ['DER_mass_jet_jet','DER_prodeta_jet_jet','PRI_jet_leading_eta',
-        'PRI_jet_leading_phi','PRI_jet_subleading_eta','PRI_jet_subleading_phi','DER_deltaeta_jet_jet',
-        'PRI_jet_subleading_pt']
-    defined_columns = [item for item in features if item not in undefined_columns]
-    """
-    train_inputs[defined_columns] = (train_inputs[defined_columns] - train_inputs[defined_columns].mean()) / train_inputs[defined_columns].std()
+    low_level_features = [
+     'PRI_tau_pt',
+     'PRI_tau_eta',
+     'PRI_tau_phi',
+     'PRI_lep_pt',
+     'PRI_lep_eta',
+     'PRI_lep_phi',
+     'PRI_met',
+     'PRI_met_phi',
+     'PRI_met_sumet',
+     'PRI_jet_num',
+     'PRI_jet_leading_pt',
+     'PRI_jet_all_pt']
+     
+    features = high_level_features + low_level_features
 
-    #first replace -999 with np.nan
-    train_inputs[undefined_columns] = train_inputs[undefined_columns].replace(-999,value=np.nan)
-
-    #standardize
-    train_inputs[undefined_columns] = (train_inputs[undefined_columns] - train_inputs[undefined_columns].mean()) / train_inputs[undefined_columns].std()
-
-    #replace np.nan with -999
-    train_inputs[undefined_columns] = train_inputs[undefined_columns].fillna(-999)
-
-    test_inputs[defined_columns] = (test_inputs[defined_columns] - test_inputs[defined_columns].mean()) / test_inputs[defined_columns].std()
-
-    #first replace -999 with np.nan
-    test_inputs[undefined_columns] = test_inputs[undefined_columns].replace(-999,value=np.nan)
-
-    #standardize
-    test_inputs[undefined_columns] = (test_inputs[undefined_columns] - test_inputs[undefined_columns].mean()) / test_inputs[undefined_columns].std()
-
-    #replace np.nan with -999
-    test_inputs[undefined_columns] = test_inputs[undefined_columns].fillna(-999)
-    """
-    train_inputs = training_data[defined_columns]
-    test_inputs = testing_data[defined_columns]
+    train_inputs = training_data[features]
+    test_inputs = testing_data[features]
 
     train_inputs = (train_inputs - train_inputs.mean())/train_inputs.std()
     test_inputs = (test_inputs - test_inputs.mean())/test_inputs.std()
